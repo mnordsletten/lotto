1123a084e3ad8e35bb265febbd2694f0d01141e4
Martin Nordsletten <mnordsletten@gmail.com>
2018-12-18 10:36:58 +0100

    Download for hyp test: Verify GRUB + verify file format

--- tests/download-image-for-hypervisors/download-image-for-hypervisors.sh
+++ tests/download-image-for-hypervisors/download-image-for-hypervisors.sh
@@ -1,6 +1,5 @@
 # download image script
 # Script used for checking that downloading an image for each hypervisor works
-set -e
 
 cutOffFileSize="10000"
 moth="{{.MothershipBinPathAndName}}"
@@ -11,29 +10,45 @@ instID=$($moth inspect-instance $instAlias -o id)
 naclID=$($moth push-nacl tests/download-image-for-hypervisors/interface.nacl {{.BuilderID}} -o id)
 imgID=$($moth build Starbase {{.BuilderID}} --instance $instID --nacl $naclID --waitAndPrint)
 
-hypervisor=""
-
 # Download the image for hypervisors qemu, vcloud and virtualbox
-for i in {1..3}
+for hypervisor in vcloud virtualbox qemu
 do
-    if [ $i == 1 ]; then
-        hypervisor="vcloud"
-    elif [ $i == 2 ]; then
-        hypervisor="virtualbox"
-    else
-        hypervisor="qemu"
-    fi
-    downloadedImgName="img-$hypervisor"
     sent=$[$sent + 1]
+    downloadedImgName="hypervisor-test-image"
     # Download image
     raw+=$($moth pull-image $imgID $downloadedImgName --format $hypervisor 2>&1)
-    img_file_size=$(du -sk $downloadedImgName | awk '{print $1}')
-    if [ "$?" -eq "0" ]; then
-      if [ "$img_file_size" -gt "$cutOffFileSize" ]; then
-        received=$[$received + 1]
-      fi
-      raw+="$downloadedImgName size: $img_file_size, is too small"
+    if [ "$?" -ne "0" ]; then
+      raw+="download of image for hypervisor $hypervisor failed"
+      continue
     fi
+
+    # Verify image size
+    img_file_size=$(du -sk $downloadedImgName | awk '{print $1}')
+    if [ "$img_file_size" -lt "$cutOffFileSize" ]; then
+      raw+="$downloadedImgName size: $img_file_size, is too small"
+      continue
+    fi
+
+    # Verify that image contains GRUB
+    strings $downloadedImgName | grep GRUB 2>&1 > /dev/null
+    if [ "$?" -ne "0" ]; then
+      raw+="image for hypervisor $hypervisor does not contain GRUB"
+      continue
+    fi
+
+    # Verify correct file format
+    case "$hypervisor" in
+      "vcloud" ) file $downloadedImgName | grep "VMware4 disk image" 2>&1 > /dev/null;;
+      "virtualbox" ) file $downloadedImgName | grep "VirtualBox Disk Image" 2>&1 > /dev/null;;
+      "qemu" ) file $downloadedImgName | grep "DOS/MBR boot sector" 2>&1 > /dev/null;;
+    esac
+    if [ "$?" -ne "0" ]; then
+      raw+="image for hypervisor $hypervisor does not seem to be correct file format"
+      raw+=$(file $downloadedImgName 2>&1)
+      continue
+    fi
+
+    received=$[$received + 1]
 done
 
 # If none of the commands above failed it means that we were successful
