d064fe4f64b6af9beb7abbb13114f263221a3260
Martin Nordsletten <mnordsletten@gmail.com>
2019-01-16 13:28:00 +0100

    testv2: Created initial gateway service + ping, download tests

--- tests/testTypes/ping/ping.sh
+++ tests/testTypes/ping/ping.sh
@@ -1,13 +1,11 @@
-# Pinger
+# Pinging 10.100.0.150
 # Script used for pinging the instance at a fixed rate
 # Returns rate and average response time
 
-# Input values to cmd
-sent=5
+TARGET={{.target}}
+sent=20
 rate=5 # Requests pr second, higher than 5 requires sudo
-raw=$(ping -c $sent -i $(awk "BEGIN {print 1/$rate}") -q 10.100.0.150)
-
-# Parse output
+raw=$(ping -c $sent -i $(awk "BEGIN {print 1/$rate}") -q $TARGET)
 received=$(printf "%s" "$raw" | grep received | cut -d ' ' -f 4)
 
 if [ "$sent" -eq "$received" ]; then
